ca71dedbd9763ff734a526606d720d58ec26be7c
luizvascolopes-design <luizvascolopes@gmail.com>
2026-02-08 18:14:03 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'fitness-lopes-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // se quiser, pode adicionar './icon-192.png', './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
